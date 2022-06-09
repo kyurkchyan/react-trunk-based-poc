5d79ed58d771046b1ae12b6a806e88baecca6040
TigranKhudav <khudav.tigran@gmail.com>
2022-06-09 16:45:33 +0400

    fix tag validation

--- .github/release.sh
+++ .github/release.sh
@@ -1,3 +1,11 @@
+re='^[0-9]+$'
+IFS='.'
+for val in $1 ; do
+    if ! [[ $val =~ $re ]] ; then
+        echo "error: Not a tag" >&2; exit 1
+    fi
+done
+
 versionTag="v$1"
 echo "$versionTag"
 git tag "$versionTag" -a -m "$versionTag"
